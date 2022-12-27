c686cb987fd3be7a9d044c9465495994ca9489e9
roki <roma135456@gmail.com>
2022-12-27 21:02:44 +0300

    Create

---
+++ SellCar/SellCar.Domain/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace SellCar.Domain
+{
+    internal class Car
+    {
+        public Guid CarId { get; set; }
+        public Guid UserId { get; set; }
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+    }
+}
