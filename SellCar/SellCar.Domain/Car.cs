24ff104fa6f43806056dd141a1a45d03006a3e94
roki <roma135456@gmail.com>
2022-12-29 00:21:21 +0300

    upd

--- SellCar/SellCar.Domain/Car.cs
+++ SellCar/SellCar.Domain/Car.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Reflection;
 
 
 namespace SellCar.Domain
 {
     internal class Car
     {
-        public int IdCar  {get; set;}
-
+        public Guid CarId { get; set; }
+        public Guid UserId { get; set; }
         public string Brand { get; set; }
-        public string Model {get; set;}
-        public string YearCreate {get; set;}
 
-        public string Typee {get; set;}
+        public string Model { get; set; }
     }
 }
